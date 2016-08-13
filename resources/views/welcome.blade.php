7c915ec2a62b9e1eb741574f269f068b8a5bbb0e
Savu <andrei.savu@webland.ro>
2016-08-13 22:04:20 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        {!! Html::style('assets/css/bootstrap/bootstrap.min.css') !!}
+        {!! Html::style('assets/css/main.css') !!}
+
+        {!! Html::script('assets/js/jquery/jquery-2.2.4.min.js') !!}
+        {!! Html::script('assets/js/bootstrap/bootstrap.min.js') !!}
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title"> Laravel 5.2.43</div>
+                <div class="info">clean install with goodies :</div>
+                <ul>
+                    <li><strong>CSS:</strong></li>
+                    <li>- Bootstrap v3.3.6  </li>
+                    <li><strong>JS:</strong></li>
+                    <li>- jQuery v2.2.4 </li>
+                    <li>- Bootstrap v3.3.6 </li>
+                    <li><strong>Packages:</strong></li>
+                    <li>- Laravel Collective 5.2.*</li>
+                    <li>- Laravel Socialite 2.0.18</li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
